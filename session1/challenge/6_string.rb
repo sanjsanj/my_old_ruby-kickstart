806d591f01adaa16dc153ec40784269543aec0fd
sanjsanj <sanjsanj@hotmail.com>
2015-02-26 20:26:17 +0000

    Complete session 1 exercises

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -8,4 +8,11 @@
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  to_return = ""
+  string.size.times do |index|
+    next if return_odds && index.even?
+    next if !return_odds && index.odd?
+    to_return << string[index]
+  end
+  to_return
 end
